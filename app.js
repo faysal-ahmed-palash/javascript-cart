471f34baa736a8079c983efd102e9a8ae4c6448e
Faysal Ahmed Palash <palash884@gmail.com>
2021-01-27 23:38:59 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-// quantity-one,increment-one,price-one,item-rate1,subtotal,final-total
 
 
-// Increment work
+
+// Increment work item one
 const incrementBtn = document.getElementById("increment-one");
 incrementBtn.addEventListener("click", function () {
     const itemRate = parseFloat(document.getElementById("rate-one").innerHTML);
@@ -15,12 +15,26 @@ incrementBtn.addEventListener("click", function () {
     document.getElementById("subtotal").innerHTML = subTotal + itemRate;
     document.getElementById("final-total").innerHTML = finalTotal + itemRate;
 
+})
 
-    //console.log(itemRate);
+// Increment work item two
+const incrementBtn2 = document.getElementById("increment-two");
+incrementBtn2.addEventListener("click", function () {
+    const itemRate2 = parseFloat(document.getElementById("rate-two").innerHTML);
+    const currentQty2 = parseFloat(document.getElementById("quantity-two").value);
+    const totalBalance2 = parseFloat(document.getElementById("price-two").innerHTML);
+    const subTotal = parseFloat(document.getElementById("subtotal").innerHTML);
+    const finalTotal = parseFloat(document.getElementById("final-total").innerHTML);
+
+    document.getElementById("quantity-two").value = currentQty2 + 1;
+    document.getElementById("price-two").innerHTML = totalBalance2 + itemRate2;
+    document.getElementById("subtotal").innerHTML = subTotal + itemRate2;
+    document.getElementById("final-total").innerHTML = finalTotal + itemRate2;
+
 })
 
 
-// decrement work
+// decrement work item one
 const decrementBtn = document.getElementById("decrement-one");
 decrementBtn.addEventListener("click", function () {
     const itemRate = parseFloat(document.getElementById("rate-one").innerHTML);
@@ -37,32 +51,44 @@ decrementBtn.addEventListener("click", function () {
 
 })
 
+// decrement work item two
+const decrementBtn2 = document.getElementById("decrement-two");
+decrementBtn2.addEventListener("click", function () {
+    const itemRate2 = parseFloat(document.getElementById("rate-two").innerHTML);
+    const currentQty2 = parseFloat(document.getElementById("quantity-two").value);
+    const totalBalance2 = parseFloat(document.getElementById("price-two").innerHTML);
+    const subTotal = parseFloat(document.getElementById("subtotal").innerHTML);
+    const finalTotal = parseFloat(document.getElementById("final-total").innerHTML);
 
-// Remove work
+    document.getElementById("quantity-two").value = currentQty2 - 1;
+    document.getElementById("price-two").innerHTML = totalBalance2 - itemRate2;
+    document.getElementById("subtotal").innerHTML = subTotal - itemRate2;
+    document.getElementById("final-total").innerHTML = finalTotal - itemRate2;
+
+
+})
+
+
+// Remove work item one
 const removetBtn = document.getElementById("remove-one");
 removetBtn.addEventListener("click", function () {
     const totalBalance = parseFloat(document.getElementById("price-one").innerHTML);
     const subTotal = parseFloat(document.getElementById("subtotal").innerHTML);
     const finalTotal = parseFloat(document.getElementById("final-total").innerHTML);
 
-
     document.getElementById("item-one").style.display = "none";
     document.getElementById("subtotal").innerHTML = subTotal - totalBalance;
     document.getElementById("final-total").innerHTML = finalTotal - totalBalance;
-
 })
 
+// Remove two work item one
+const removetBtn2 = document.getElementById("remove-two");
+removetBtn2.addEventListener("click", function () {
+    const totalBalance = parseFloat(document.getElementById("price-two").innerHTML);
+    const subTotal = parseFloat(document.getElementById("subtotal").innerHTML);
+    const finalTotal = parseFloat(document.getElementById("final-total").innerHTML);
 
-// Deposit button event
-// const depositBtn = document.getElementById("deposit-btn");
-// depositBtn.addEventListener("click", function () {
-
-//     const depositAmmount = parseFloat(document.getElementById("deposit-amt").value);
-//     const currDeposit = parseFloat(document.getElementById("curr-deposit").innerHTML);
-//     const totalBalance = parseFloat(document.getElementById("total-balance").innerHTML);
-
-//     document.getElementById("curr-deposit").innerHTML = currDeposit + depositAmmount;
-//     document.getElementById("total-balance").innerHTML = totalBalance + depositAmmount;
-//     document.getElementById("deposit-amt").value = "";
-
-// })
+    document.getElementById("item-two").style.display = "none";
+    document.getElementById("subtotal").innerHTML = subTotal - totalBalance;
+    document.getElementById("final-total").innerHTML = finalTotal - totalBalance;
+})
